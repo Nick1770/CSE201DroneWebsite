d5220c5a02343e8913ee3f228efd009d47abfb31
Nickolas DiSisto <nick@bis-172-22-91-101.dhcp.muohio.edu>
2022-11-12 14:40:26 -0500

    Pls work

---
+++ React-Website/React-Website/components/Header.js
@@ -0,0 +1,162 @@
+import React from "react";
+import { scroller } from "react-scroll";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+  useMediaQuery,
+  Tab,
+  Tooltip,
+} from "@material-ui/core";
+import { Menu as MenuIcon, Brightness4, Brightness7 } from "@material-ui/icons";
+
+import logo from "./DroneLogo.jpeg";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    [theme.breakpoints.down("xs")]: {
+      flexGrow: 1,
+    },
+  },
+  headerOptions: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "space-evenly",
+  },
+}));
+
+const Header = (props) => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
+  const { darkMode, setDarkMode } = props;
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const scrollToSection = (section) => {
+    scroller.scrollTo(section, {
+      duration: 500,
+      delay: 0,
+      smooth: "easeInOutQuart",
+    });
+  };
+
+  const handleClickAndClose = (sectionID) => {
+    setAnchorEl(null);
+    scrollToSection(sectionID);
+  };
+
+  const sections = [
+    {
+      menuTitle: "About",
+      sectionID: "About",
+    },
+   
+    {
+      menuTitle: "Calendar",
+      sectionID: "Calendar",
+    },
+    {
+      menuTitle: "Drone Racing",
+      sectionID: "Course",
+    },
+    
+    {
+      menuTitle: "Leadership",
+      sectionID: "Leadership",
+    },
+    {
+      menuTitle: "Newsletter",
+      sectionID: "Newsletter",
+    },
+  ];
+
+  const tabs = sections.map((menuItem, index) => {
+    const { menuTitle, sectionID } = menuItem;
+    return (
+      <Tab
+        key={index}
+        onClick={() => scrollToSection(sectionID)}
+        label={<Typography variant="body1">{menuTitle}</Typography>}
+      />
+    );
+  });
+
+  const menuItems = sections.map((menuItem, index) => {
+    const { menuTitle, sectionID } = menuItem;
+    return (
+      <MenuItem onClick={() => handleClickAndClose(sectionID)} key={index}>
+        {menuTitle}
+      </MenuItem>
+    );
+  });
+
+  const darkLightModeButton = (
+    <Tooltip title="Toggle light/dark mode">
+      <IconButton color="secondary" onClick={() => setDarkMode(!darkMode)}>
+        {darkMode ? <Brightness7 /> : <Brightness4 />}
+      </IconButton>
+    </Tooltip>
+  );
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          {isMobile ? (
+            <>
+              <img src={logo} width="auto" height="100" alt="MUBC Logo" />
+              <div style={{ marginLeft: "auto" }}>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  onClick={handleMenu}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={open}
+                  onClose={() => setAnchorEl(null)}
+                >
+                  {menuItems}
+                  {setDarkMode(false)}
+                </Menu>
+              </div>
+            </>
+          ) : (
+            <>
+              <img src={logo} width="auto" height="100" alt="MUBC Logo" />
+              {tabs}
+              {darkLightModeButton}
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
