7d6d37bdb20e879f7f4478dd04c1ee4e8af598c4
Braedan Russell <russelb7@miamioh.edu>
2022-11-13 14:10:54 -0500

    About Us

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -9,8 +9,14 @@ const Home = () => {
             <div data-src="/images/leFish.png"/>
             <div data-src="/images/Ligma.png" />
             </AwesomeSlider>
-            {/* <img src='/images/leFish.png' alt="image not found"/>
-            <img src='/images/Ligma.png' alt="image not found"/> */}
+
+            <h1>About Us!</h1>
+            <d> The Miami University Drone Club is dedicated to flying drones,
+                building drones, and spreading general knowledge about drones.
+                We have in class drone races, attend seminars, have drone workshops
+                every Wednesday and have plenty of oppurtunities to meet and connect
+                with fellow enthusiasts. We will never charge club dues and are
+                always open to new members! No knowledge necessary.</d>
         </>
     )
 }
