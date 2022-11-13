ebfdc76c8abc732925fc067ac56c9d2a27e058b8
William Shuppert <55960123+williamShuppert@users.noreply.github.com>
2022-11-13 14:11:10 -0500

    style image slider

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,14 +1,17 @@
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
+import './home.css'
 
 const Home = () => {
     return (
         <>
             <h1>Home</h1>
-            <AwesomeSlider animation="cubeAnimation">
-            <div data-src="/images/leFish.png"/>
-            <div data-src="/images/Ligma.png" />
-            </AwesomeSlider>
+            <div class="slider-container">
+                <AwesomeSlider animation="cubeAnimation">
+                    <div data-src="/images/leFish.png"/>
+                    <div data-src="/images/Ligma.png" />
+                </AwesomeSlider>
+            </div>
             {/* <img src='/images/leFish.png' alt="image not found"/>
             <img src='/images/Ligma.png' alt="image not found"/> */}
         </>
