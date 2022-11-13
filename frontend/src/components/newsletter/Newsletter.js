1f6c07f7d5f0df757c52fe7bd72ab3ea47b85604
Nickolas DiSisto <nick@bis-172-22-91-101.dhcp.muohio.edu>
2022-11-13 14:42:29 -0500

    Newsletter

---
+++ frontend/src/components/newsletter/Newsletter.js
@@ -0,0 +1,17 @@
+const Newsletter = () => {
+    return (
+        <>
+            <div>
+
+                <h1>NewsLetter!</h1>
+                <d> Click here to join our news letter.</d>
+
+
+                
+
+            </div>
+        </>
+    )
+}
+
+export default Newsletter
