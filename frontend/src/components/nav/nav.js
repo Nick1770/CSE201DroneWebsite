b2009e822c679ef6cc5098413ce1545e9d222ef3
Nickolas DiSisto <nick@bis-172-22-91-101.dhcp.muohio.edu>
2022-11-13 13:38:18 -0500

    Dark Mode

--- frontend/src/components/nav/nav.js
+++ frontend/src/components/nav/nav.js
@@ -1,10 +1,21 @@
 import { Link } from "react-router-dom"
 import useAuth from "../../hooks/useAuth"
 import "./nav.css"
+import React, { useState, useEffect } from 'react';
 
 export const Nav = () => {
     const { auth, logout, hasRole } = useAuth()
-
+    const [theme, setTheme] = useState('light');
+    const toggleTheme = () => {
+        if (theme === 'light') {
+        setTheme('dark');
+        } else {
+        setTheme('light');
+        }
+};
+    useEffect(() => {
+    document.body.className = theme;
+    }, [theme]);
     return (
         <nav>
             <Link to="/">Home</Link>
@@ -22,6 +33,7 @@ export const Nav = () => {
                             <Link to="/register">Register</Link>
                         </>
                 }
+             <Link onClick={toggleTheme}>DarkMode</Link>
             </div>
         </nav>
     )
