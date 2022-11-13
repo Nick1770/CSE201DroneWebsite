efc2209578f88f4aec1b86c167a693df3b825504
Braedan Russell <russelb7@miamioh.edu>
2022-11-13 13:03:22 -0500

    Image test

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,8 +1,10 @@
+import coding from "./leFish.png";
 
 const Home = () => {
     return (
         <>
             <h1>Home</h1>
+            <img src={coding} alt="BigCo Inc. logo"/>
         </>
     )
 }
