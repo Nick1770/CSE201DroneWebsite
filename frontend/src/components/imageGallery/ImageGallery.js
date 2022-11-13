ebd9ff461dd3afa251868b8525ad5eaf550a9d30
Braedan Russell <russelb7@miamioh.edu>
2022-11-13 16:01:46 -0500

    ImageGallery Final Final v2

--- frontend/src/components/imageGallery/ImageGallery.js
+++ frontend/src/components/imageGallery/ImageGallery.js
@@ -6,12 +6,10 @@ const ImageGallery = () => {
         <>
             <div class="slider-container">
                     <AwesomeSlider animation="cubeAnimation">
-                        <div data-src="/images/group.jpg" />
-                        <div data-src="/images/coding.jpg" />
-                        <div data-src="/images/presentation.jpg" />
-                        <div data-src="/images/group1.jpg" />
-                        <div data-src="/images/tShirt1.jpg" />
-                        <div data-src="/images/tShirt2.jpg" />
+                        <div data-src="/images/Drone1.jpg" />
+                        <div data-src="/images/Drone2.jpg" />
+                        <div data-src="/images/Drone3.jpg" />
+                        <div data-src="/images/DroneRace.png" />
                     </AwesomeSlider>
                 </div>
         </>
