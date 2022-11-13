42ca2906b41a5148ee4f3ad0f79efb98f5405760
Braedan Russell <russelb7@miamioh.edu>
2022-11-13 12:06:09 -0500

    image gallery

---
+++ frontend/src/components/imageGallery/imageGallery.js
@@ -0,0 +1,66 @@
+// import AwesomeSlider from "react-awesome-slider";
+// import { Grid } from "@material-ui/core";
+// import { makeStyles } from "@material-ui/core/styles";
+// import "react-awesome-slider/dist/styles.css";
+
+// import presentationImage from "./images/presentation.jpg";
+// import groupImage from "./images/group.jpg";
+// import firstTShirtImage from "./images/tShirt1.jpg";
+// import secondTShirtImage from "./images/tShirt2.jpg";
+// import codingImage from "./images/coding.jpg";
+
+// const useStyles = makeStyles((theme) => ({
+//   gallery: {
+//     height: "80%",
+//     marginTop: "3%",
+//   },
+// }));
+
+// const ImageGallery = () => {
+//   const classes = useStyles();
+//   return (
+//     <Grid item container>
+//       <Grid item xs={1} />
+//       <Grid item xs={10}>
+//         <AwesomeSlider
+//           className={classes.gallery}
+//           media={[
+//             {
+//               source: groupImage,
+//             },
+//             {
+//               source: presentationImage,
+//             },
+//             {
+//               source: firstTShirtImage,
+//             },
+//             {
+//               source: codingImage,
+//             },
+//             {
+//               source: secondTShirtImage,
+//             },
+//           ]}
+//         />
+//       </Grid>
+//       <Grid item xs={1} />
+//     </Grid>
+//   );
+// };
+
+// export default ImageGallery;
+
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+ 
+const ImageGallery = () => {
+  return (
+     <AwesomeSlider animation="cubeAnimation">
+       <div data-src="/path/to/coding.jpg" />
+       <div data-src="/path/to/image-1.png" />
+       <div data-src="/path/to/image-2.jpg" />
+     </AwesomeSlider>
+  )
+  };
+
+  export default ImageGallery
