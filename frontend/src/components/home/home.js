c85448885aca3456292ce9c2782d24d24f2a4c16
William Shuppert <55960123+williamShuppert@users.noreply.github.com>
2022-11-13 14:14:25 -0500

    style home page

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -5,21 +5,23 @@ import './home.css'
 const Home = () => {
     return (
         <>
-            <h1>Home</h1>
-            <div class="slider-container">
-                <AwesomeSlider animation="cubeAnimation">
-                    <div data-src="/images/leFish.png"/>
-                    <div data-src="/images/Ligma.png" />
-                </AwesomeSlider>
-            </div>
+            <div class="wrapper">
+                <h1>Home</h1>
+                <div class="slider-container">
+                    <AwesomeSlider animation="cubeAnimation">
+                        <div data-src="/images/leFish.png"/>
+                        <div data-src="/images/Ligma.png" />
+                    </AwesomeSlider>
+                </div>
 
-            <h1>About Us!</h1>
-            <d> The Miami University Drone Club is dedicated to flying drones,
-            building drones, and spreading general knowledge about drones.
-            We have in class drone races, attend seminars, have drone workshops
-            every Wednesday and have plenty of oppurtunities to meet and connect
-            with fellow enthusiasts. We will never charge club dues and are
-            always open to new members! No knowledge necessary.</d>
+                <h1>About Us!</h1>
+                <d> The Miami University Drone Club is dedicated to flying drones,
+                building drones, and spreading general knowledge about drones.
+                We have in class drone races, attend seminars, have drone workshops
+                every Wednesday and have plenty of oppurtunities to meet and connect
+                with fellow enthusiasts. We will never charge club dues and are
+                always open to new members! No knowledge necessary.</d>
+            </div>
         </>
     )
 }
