0150aeda7c2629a39743ade183d5cf76ea44d1b6
William Shuppert <55960123+williamShuppert@users.noreply.github.com>
2022-11-14 12:22:54 -0500

    delete events from frontend

--- frontend/src/components/calendar/Calendar.js
+++ frontend/src/components/calendar/Calendar.js
@@ -25,20 +25,19 @@ const Calendar = (props) => {
     }
 
     const handleAddEvent = async (event) => {
-      await callAPI('/events', 'POST', event)
+      const res = await callAPI('/events', 'POST', event)
+      const json = await res.json()
+      event.id = json.id
       setEvents(prev => [...prev, event])
     }
   
-    const eventClick = function (info) {
+    const eventClick = async (info) => {
       info.jsEvent.preventDefault();
-  
-      if (info.event.url) {
-        window.open(info.event.url);
-      }
 
-      console.log('clicked on event')
-      console.log(info.event.id)
-      // TODO: open popup to delete event if user is admin
+      if (window.confirm(`Are you sure you want to delete this event? ${info.event.title}`)) {
+        await callAPI(`/events/${info.event.id}`, 'DELETE')
+        setEvents(prev => prev.filter(e => e.id !== parseInt(info.event.id)))
+      }
     };
   
     return (
