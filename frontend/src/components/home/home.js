384ddf6b17d6fa527978c58f11a0c48ef38a3388
William Shuppert <55960123+williamShuppert@users.noreply.github.com>
2022-11-13 23:25:58 -0500

    fix react warnings

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -5,20 +5,18 @@ import ImageGallery from '../imageGallery/ImageGallery';
 const Home = () => {
     return (
         <>
-            <div class="wrapper">
-                <h1>Welcome to MU Drone Club</h1>
-                <ImageGallery/>
+            <h1>Welcome to MU Drone Club</h1>
+            <ImageGallery/>
 
-                <h1>About Us!</h1>
-                <d> The Miami University Drone Club is dedicated to flying drones,
-                building drones, and spreading general knowledge about drones.
-                We have in class drone races, attend seminars, have drone workshops
-                every Wednesday and have plenty of oppurtunities to meet and connect
-                with fellow enthusiasts. We will never charge club dues and are
-                always open to new members! No knowledge necessary.</d>
+            <h1>About Us!</h1>
+            <p> The Miami University Drone Club is dedicated to flying drones,
+            building drones, and spreading general knowledge about drones.
+            We have in class drone races, attend seminars, have drone workshops
+            every Wednesday and have plenty of opportunities to meet and connect
+            with fellow enthusiasts. We will never charge club dues and are
+            always open to new members! No knowledge necessary.</p>
 
-                <Newsletter/>
-            </div>
+            <Newsletter/>
         </>
     )
 }
--- frontend/src/components/imageGallery/ImageGallery.js
+++ frontend/src/components/imageGallery/ImageGallery.js
@@ -4,15 +4,15 @@ import 'react-awesome-slider/dist/styles.css';
 const ImageGallery = () => {
     return (
         <>
-            <div class="slider-container">
-                    <AwesomeSlider animation="cubeAnimation">
-                        <div data-src="/images/Drone1.jpg" />
-                        <div data-src="/images/Drone2.jpg" />
-                        <div data-src="/images/Drone3.jpg" />
-                        <div data-src="/images/DroneRace.png" />
-                        <div data-src="/images/DroneFootage.mp4" />
-                    </AwesomeSlider>
-                </div>
+            <div className="slider-container">
+                <AwesomeSlider animation="cubeAnimation">
+                    <div data-src="/images/Drone1.jpg" />
+                    <div data-src="/images/Drone2.jpg" />
+                    <div data-src="/images/Drone3.jpg" />
+                    <div data-src="/images/DroneRace.png" />
+                    <div data-src="/images/DroneFootage.mp4" />
+                </AwesomeSlider>
+            </div>
         </>
     )
 }
